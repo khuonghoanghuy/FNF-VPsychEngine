f56cdbce35f49dabdd2ce38f5db44ce616c406d0
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-07-08 09:38:40 +0700

    Create CodePad.hx

---
+++ source/editors/CodePad.hx
@@ -0,0 +1,19 @@
+package editors;
+
+import flixel.FlxG;
+
+// a state allows u coding inside the game, wow!
+class CodePad extends MusicBeatState
+{
+  var str:String = "write code";
+  
+  override function create()
+  {
+    super.create();
+  }
+
+  override function update(elapsed:Float)
+  {
+    super.update(elapsed);
+  }
+}
