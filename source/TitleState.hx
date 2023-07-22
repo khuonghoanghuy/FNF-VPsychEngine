d3e3e98f37dcdffc81eef661bd84241dff792870
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2023-07-22 23:19:41 +0700

    warm stuff

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -135,14 +135,22 @@ class TitleState extends MusicBeatState
 			trace('checking for update');
 			var http = new haxe.Http("https://raw.githubusercontent.com/khuonghoanghuy/FNF-VPsychEngine/main/gitVersion.txt");
 
+			// for in-develop build stuff
+			var idkWhy:Bool = true;
+
 			http.onData = function (data:String)
 			{
 				updateVersion = data.split('\n')[0].trim();
 				var curVersion:String = MainMenuState.VPsychVer.trim();
 				trace('version online: ' + updateVersion + ', your version: ' + curVersion);
-				if(updateVersion != curVersion) {
+				if(updateVersion != curVersion && !idkWhy) {
 					trace('versions arent matching!');
 					mustUpdate = true;
+					OutdatedState.exWarm = false;
+				} else if (updateVersion != curVersion && idkWhy) {
+					trace('version arent macthing! but something wrong');
+					mustUpdate = true;
+					OutdatedState.exWarm = true;
 				}
 			}
 
